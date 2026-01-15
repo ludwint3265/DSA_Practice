6bf60c195f1f14dad364ac9118c5568508b580c6
Ludwin Tenezaca <ludwin.tenezaca38@myhunter.cuny.edu>
2026-01-14 23:16:02 -0500

    added validAnagram and hasDuplicate code

---
+++ dataStructures/leetcodePractice.py
@@ -0,0 +1,38 @@
+import string
+
+def isAnagram(self, s: str, t: str) -> bool:
+        #if the lengths of the strings are different, they cannot be anagrams
+        if len(s) != len(t):
+            return False
+        else:
+            seen = {}
+
+            #adding each character at its respective index to their own hash maps
+            #done to compare frequency for each character
+            for char in s:
+                if char not in seen:
+                    seen[char]=1
+                else:
+                    seen[char]+=1
+            
+            seenT = {}
+            for charT in t:
+                if charT not in seenT:
+                    seenT[charT]=1
+                else:
+                    seenT[charT]+=1
+            
+            #Python dict comparison checks for :
+            #total dict length + exact key-value matches (all keys present, with same exact values for each one)
+            #no need to manually check all values and their amounts
+            if seen != seenT:
+                return False
+        return True
+
+def hasDuplicate(self, nums: list[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
