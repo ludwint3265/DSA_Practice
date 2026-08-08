4c140a056c1ce872969bf8ea8dffbcb25effed40
Ludwin Tenezaca <ludwin.tenezaca38@myhunter.cuny.edu>
2026-08-08 13:15:01 -0400

    added two practice questions for accessing custom objects and their data members

--- linkedListPractice.py
+++ linkedListPractice.py
@@ -268,3 +268,163 @@ jigglypuff.print_pokemon()
 jigglypuff.add_type("Fairy")
 jigglypuff.print_pokemon()
 print("\n")
+
+'''
+Problem 7
+
+Outside the Pokemon class, write a new function get_by_type() that takes in a list of Pokemon instances my_pokemon and a string pokemon_type as parameters.
+
+The function should return a list of all Pokemon instances from my_pokemon that have the type pokemon_type.
+
+Hint: To test, loop over Pokemon in return list and print the Pokemon's name
+
+class Pokemon():
+	...
+	
+def get_by_type(my_pokemon, pokemon_type):
+	pass
+Example Usage:
+
+# initializing pokemons
+jigglypuff = Pokemon("Jigglypuff", ["Normal", "Fairy"])
+diglett = Pokemon("Diglett", ["Ground"])
+meowth = Pokemon("Meowth", ["Normal"])
+pidgeot = Pokemon("Pidgeot", ["Normal", "Flying"])
+blastoise = Pokemon("Blastoise", ["Water"])
+
+my_pokemon = [jigglypuff, diglett, meowth, pidgeot, blastoise]
+normal_pokemon = get_by_type(my_pokemon, "Normal")
+print(normal_pokemon)
+Example Output: [Jigglypuff, Meowth, Pidgeot]
+'''
+
+class Pokemon:
+    def __init__(self, name, types):
+        self.name = name
+        self.types = types
+        self.is_caught = False
+
+    def print_pokemon(self):
+         print({
+            "name": self.name,   
+            "types": self.types, 
+            "is_caught": self.is_caught 
+         })
+
+    def catch(self):
+        self.is_caught = True
+
+    def choose(self):
+        if self.is_caught == True:
+            print(f"{self.name} I choose you!")
+        else:
+            print(f"{self.name} is wild! Catch them if you can!")
+
+    def add_type(self, new_type):
+        if new_type is None or isinstance(new_type, str) == False:
+            print("Could not add a None type / non-string object to types, please enter a string")
+        else:
+            self.types.append(new_type)
+
+def get_by_type(my_pokemon, pokemon_type):
+    if my_pokemon is None or pokemon_type is None or len(my_pokemon) == 0 or isinstance(my_pokemon, list) == False:
+        return []
+    else:
+        matching_pokemon = []
+        for pokemon in my_pokemon:
+            for type in pokemon.types:
+                if type == pokemon_type:
+                    matching_pokemon.append(pokemon.name)
+
+        return matching_pokemon
+
+print("Question 7:\n")
+jigglypuff = Pokemon("Jigglypuff", ["Normal", "Fairy"])
+diglett = Pokemon("Diglett", ["Ground"])
+meowth = Pokemon("Meowth", ["Normal"])
+pidgeot = Pokemon("Pidgeot", ["Normal", "Flying"])
+blastoise = Pokemon("Blastoise", ["Water"])
+
+my_pokemon = [jigglypuff, diglett, meowth, pidgeot, blastoise]
+normal_pokemon = get_by_type(my_pokemon, "Normal")
+print(normal_pokemon)
+print("\n")
+
+'''
+Problem 8:
+Some Pokemon can evolve into other species of Pokemon.
+In the updated Pokemon class below, each instance of Pokemon has an attribute evolution. 
+The attribute will either be the default value of None or another Pokemon instance.
+
+Write a function get_evolutionary_line() that takes in a 
+Pokemon object starter_pokemon as a parameter.
+
+The function should return a list of itself and the Pokemon that the
+starter_pokemon can evolve into.
+
+class Pokemon():
+	def  __init__(self, name, types, evolution = None):
+		self.name = name
+		self.types = types
+		self.is_caught = False
+		self.evolution = evolution
+ 
+def get_evolutionary_line(starter_pokemon):
+	pass
+Example Usage:
+
+charizard = Pokemon("Charizard", ["fire", "flying"])
+charmeleon = Pokemon("Charmeleon", ["fire"], charizard)
+charmander = Pokemon("Charmander", ["fire"], charmeleon)
+
+charmander_list = get_evolutionary_line(charmander)
+print(charmander_list)
+
+charmeleon_list = get_evolutionary_line(charmeleon)
+print(charmeleon_list)
+
+charizard_list = get_evolutionary_line(charizard)
+print(charizard_list)
+Example Output:
+
+[`Charmander`, `Charmeleon`, `Charizard`]
+[`Charmeleon`, `Charizard`]
+['Charizard']
+'''
+
+print("Question 8:\n")
+class Pokemon():
+	def __init__(self, name, types, evolution = None):
+		self.name = name
+		self.types = types
+		self.is_caught = False
+		self.evolution = evolution
+
+def get_evolutionary_line(starter_pokemon):
+    if starter_pokemon is None:
+        return None
+    else:
+        pokemon_list = []
+        pokemon_list.append(starter_pokemon.name)
+        if starter_pokemon.evolution == None:
+            return pokemon_list
+        else:
+            pokemon_list.append(starter_pokemon.evolution.name)
+            if starter_pokemon.evolution.evolution is not None:
+                pokemon_list.append(starter_pokemon.evolution.evolution.name)
+                return pokemon_list
+            return pokemon_list
+
+charizard = Pokemon("Charizard", ["fire", "flying"])
+charmeleon = Pokemon("Charmeleon", ["fire"], charizard)
+charmander = Pokemon("Charmander", ["fire"], charmeleon)
+
+charmander_list = get_evolutionary_line(charmander)
+print(charmander_list)
+
+charmeleon_list = get_evolutionary_line(charmeleon)
+print(charmeleon_list)
+
+charizard_list = get_evolutionary_line(charizard)
+print(charizard_list)
+print("\n")
